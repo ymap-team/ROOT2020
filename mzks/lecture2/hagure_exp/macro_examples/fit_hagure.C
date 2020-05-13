7dc968e8ce692702fc43c571ccdb8a718f358809
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2020-05-13 18:02:58 +0900

    fix file path

--- mzks/lecture2/hagure_exp/macro_examples/fit_hagure.C
+++ mzks/lecture2/hagure_exp/macro_examples/fit_hagure.C
@@ -7,16 +7,37 @@
 // > root -l fit_hagure.C
 // ---------------------------- //
 
-int fit_hagure(){
+void fill_from_file(TH1D* hist, TString filename = "../data/data1.txt"){
 
-    gROOT->SetStyle("ATLAS");
-
-    TH1D* hist = new TH1D("hist", "hist", 25+1, 0-0.5, 25+0.5);
-    ifstream ifs("./haguremetal_escape.txt");
+    ifstream ifs(filename);
     int n_escaped_turn;
     while(ifs >> n_escaped_turn){
         hist->Fill(n_escaped_turn);
     }
+}
+
+TH1D* fill_from_files(){
+
+    int total_bin = 25;
+    int min_bin = 0;
+    int max_bin = 25;
+    TH1D* hist = new TH1D("hist", "Title", total_bin, min_bin-0.5, max_bin-0.5);
+    
+    fill_from_file(hist, "../data/data1.txt");
+    fill_from_file(hist, "../data/data2.txt");
+    fill_from_file(hist, "../data/data3.txt");
+    fill_from_file(hist, "../data/data4.txt");
+    fill_from_file(hist, "../data/data5.txt");
+
+    return hist;
+
+}
+
+int fit_hagure(){
+
+    gROOT->SetStyle("ATLAS");
+
+    TH1D* hist = fill_from_files();
     hist->Draw("E");
     hist->SetTitle(";n turn which the enemy escaped;Counts/turn");
     // hist->Rebin(2);
